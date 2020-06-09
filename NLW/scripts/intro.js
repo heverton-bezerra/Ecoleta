b3aa332131dc7f1c3c064aee21d2c88aa091dfaf
heverton-bezerra <52475975+heverton-bezerra@users.noreply.github.com>
2020-06-09 17:06:48 -0300

    Add files via upload

---
+++ NLW/scripts/intro.js
@@ -0,0 +1,70 @@
+//comments
+//document-write("Hello")
+
+// Variaveis, Tipos de dados
+//let myvar = "HE"
+//const myconst = "LLO"
+
+//document.write(myconst + myvar)
+
+//string
+//"isso é uma string"
+//'isso tambem e uma string'
+//`isso e uma string tambem`
+
+//number
+//const n1 = 1
+//const n2 = 12
+//document.write(n1 + n2)
+
+//boolean - true ou false
+//const bTrue = true
+//const bFalse = false
+//document.write(bFalse)
+
+//Objeto possui propriedades e funcionalidade
+//const pessoa = {
+// altura: "1,80",
+// idade: 24,
+// solteiro: true,
+//correr(){
+//  document write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array Vetores
+// const colecaoDeBolinhas = ["blue","greem", 1, {name: "My Name"}]
+
+//document.write(colecaoDeBolinhas[3].name)
+
+//Funçoes, Funcionalidades, atalhos - reuso de codigo
+
+// // registrar função
+// function sayMyName(name){
+
+//document.write(name)
+// }
+
+// // executar 
+// sayMyName("Douglas")
+// sayMyName("Vlaleska")
+// sayMyName("Kian")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5) {
+//      document.write("Reprovado")
+// } else {
+ //     document.write("Aprovado")
+// }
+
+// loop repetições
+// for (i = 0; i < 10; i++) {
+//    document.write(`<p>${i}</p>`) 
+//}
+
+
